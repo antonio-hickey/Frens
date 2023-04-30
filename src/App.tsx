148ae214f4052b330a441d4d7f6cbc7a3537b1b7
Antonio Hickey <antoniohickey99@gmail.com>
2023-04-30 01:07:55 -0400

    profile, feed, and event creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,99 @@
+import { 
+	generatePrivateKey, getPublicKey, relayInit, 
+	SimplePool, Event, getEventHash, signEvent,
+	Relay,
+} from "nostr-tools";
+import {useProfile} from "nostr-react";
+import { useState, useEffect } from "react";
 import './App.css';
+import CreatePostCard from "./components/createPostCard";
+
+
+
+export const RELAYS = [
+  "wss://nostr-pub.wellorder.net",
+  "wss://nostr.drss.io",
+  "wss://nostr.swiss-enigma.ch",
+  "wss://relay.damus.io",
+];
 
 
 function App() {
+	const [pool, setPool] = useState<SimplePool | null>(null);
+	const [events, setEvents] = useState<Event[]>([]);
+	const [relay, setRelay] = useState<Relay | null>(null);
+
+	//const [sk, setSk] = useState(generatePrivateKey())
+	//const [pk, setPk] = useState(getPublicKey(sk))
+	
+	/*
+		* Login Flow:
+			* User paste in their private key and we derive their 
+  			public key from the their private key.
+	*/
+
+	const [sk, setSk] = useState<string>("1a891edda8301d105145ed4514fb47b50fa6510982c74913723157019c461096")
+	const [pk, setPk] = useState<string>(getPublicKey(sk))
+	const {data: userData} = useProfile({pubkey: pk});
+
+	
+	const [pubStatus, setPubStatus] = useState("");
+
+	useEffect(() => {
+		const connectRelay = async () => {
+			const relay = relayInit('wss://relay.damus.io');
+			await relay.connect();
+
+			relay.on("connect", () => {
+				setRelay(relay);
+			})
+			relay.on("error", () => {
+				console.log('failed to connect to relay')
+			})
+		}
+
+		connectRelay();
+
+		if (events) {
+			console.log(events)
+		}
+
+		console.log(61, userData);
+		if (userData) {
+			console.log(61, userData);
+		}
+
+	}, [events]);
+
+	const event: Event = {
+		kind: 1,
+		pubkey: pk,
+		created_at: Math.floor(Date.now() / 1000),
+		content: "We are testing nostr in react",
+		tags: [],
+	}
+	event.id = getEventHash(event);
+	event.sig = signEvent(event, sk);
+
+
+	const publishEvent = (event: Event) => {
+		const pub = relay.publish(event);
+		pub.on("ok", () => {
+			setPubStatus("our event is published");
+		});
+		pub.on("failed", reason => {
+			setPubStatus("our event was NOT published " + reason);
+		})
+	}
+
+	const getEvents = async () => {
+		let events = await relay.list([{
+			kinds: [1],
+		}]);
+		setEvents(events);
+	}
+
+
   return (
 		<div className="w-screen h-screen">
 			<div className="flex flex-col w-screen">
@@ -17,11 +109,24 @@ function App() {
 				</div>
 				
 				<div className="flex flex-row">
-					<div className="flex flex-col">
-					
+					<div className="flex flex-col w-2/6 h-screen px-5">
+						{relay ? <CreatePostCard 
+							relay={relay}
+							posterPK={pk}  
+							posterSK={sk}  
+							posterName={userData?.name!} 
+							posterPicUrl={userData?.picture!}
+							publishEvent={publishEvent}
+						/>: <></>}
 					</div>
-					<div className="flex flex-col">
-
+					<div className="flex flex-col w-4/6">
+						{relay ? (
+							<div>
+								<button onClick={() => publishEvent(event)}>Publish Event</button>
+								<button onClick={() => getEvents()}>Load Feed!</button>
+							</div>
+						): <p>Failed to connect to relay</p>}
+						<p>{pubStatus}</p>
 					</div>
 				</div>
 			</div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { NostrProvider } from "nostr-react";
+
+export const RELAYS = [
+  "wss://nostr-pub.wellorder.net",
+  "wss://nostr.drss.io",
+  "wss://nostr.swiss-enigma.ch",
+  "wss://relay.damus.io",
+];
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+		<NostrProvider relayUrls={RELAYS} debug={true}>
+    	<App />
+		</NostrProvider>
   </React.StrictMode>,
 )
