8467d17cf92196c2370c3cd047f5b4cc75f0614f
Antonio Hickey <antoniohickey99@gmail.com>
2023-04-29 21:23:43 -0400

    replace vite init page with a basic starter layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+		<div className="w-screen h-screen">
+			<div className="flex flex-col w-screen">
+				<div className="flex flex-row w-screen h-10">
+      		<div className="flex w-full flex-col items-center justify-center">
+      		  <div className="relative mb-8 text-4xl md:text-6xl font-bold">
+      		    Frens
+      		    <sup className="absolute top-0 left-full text-base text-green-300">
+      		      :)
+      		    </sup>
+      		  </div>
+					</div>
+				</div>
+				
+				<div className="flex flex-row">
+					<div className="flex flex-col">
+					
+					</div>
+					<div className="flex flex-col">
+
+					</div>
+				</div>
+			</div>
+		</div>
   )
 }
 
